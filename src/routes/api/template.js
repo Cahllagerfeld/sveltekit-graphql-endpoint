4a5fa2c449f5490145e953b4c3d7c93d6b77a552
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-14 13:18:53 +0000

    feat: set up template

---
+++ src/routes/api/template.js
@@ -0,0 +1,17 @@
+import lodash from 'lodash/string';
+import fs from 'fs';
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function get() {
+	const template = lodash.template(readSDLTemplate().toString(), {});
+	console.log(template);
+	return {
+		status: 200
+	};
+}
+
+const readSDLTemplate = () => {
+	return fs.readFileSync(`${process.cwd()}/src/lib/templates/sdl.js.template`, {
+		encoding: 'utf8'
+	});
+};
