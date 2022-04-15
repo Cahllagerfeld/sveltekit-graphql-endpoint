5e95248f76257c60581900851e6471ddccdf5efa
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-15 20:20:20 +0000

    feat: glob sdl & services

--- src/routes/api/graphql.js
+++ src/routes/api/graphql.js
@@ -1,5 +1,15 @@
 import { createServer } from '@graphql-yoga/common';
 
+const sdls = Object.entries(import.meta.globEager('/src/lib/**/*.sdl.js')).map(([_, sdl]) => {
+	return { ...sdl };
+});
+
+const services = Object.entries(import.meta.globEager('/src/lib/**/*.sdl.js')).map(
+	([_, service]) => {
+		return { ...service };
+	}
+);
+
 const yogaApp = createServer({
 	schema: {
 		typeDefs: `
