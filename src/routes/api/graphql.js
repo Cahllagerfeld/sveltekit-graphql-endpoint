7cb3a93c97426d6c5bff98481de87c72e9a603fc
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-16 20:06:39 +0000

    feat: init merge resolvers

--- src/routes/api/graphql.js
+++ src/routes/api/graphql.js
@@ -1,5 +1,6 @@
 import { createServer } from '@graphql-yoga/common';
-import { mergeTypeDefs } from '@graphql-tools/merge';
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
+import merge from 'lodash/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
 const sdls = Object.entries(import.meta.globEager('/src/lib/**/*.sdl.js')).map(([_, sdl]) => {
@@ -26,8 +27,11 @@ export { yogaApp as get, yogaApp as post };
 function createSchema(sdls, services) {
 	const merged = mergeTypeDefs(sdls);
 	const schema = makeExecutableSchema({ typeDefs: merged });
-
+	createResolversFromServices(schema, services, mergeResolvers(sdls));
 	return schema;
 }
 
-function createResolversFromServices() {}
+function createResolversFromServices(schema, services, resolvers) {
+	const merged = merge({}, ...Object.keys(services).map((serviceName) => services[serviceName]));
+	console.log({ merged });
+}
