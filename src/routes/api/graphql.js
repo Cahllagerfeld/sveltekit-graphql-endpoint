bdc33a583127dd3447b61e5cdecf1bc2276407a0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-18 18:00:45 +0000

    fix: add logic, that should get generated

--- src/routes/api/graphql.js
+++ src/routes/api/graphql.js
@@ -2,6 +2,7 @@ import { createServer } from '@graphql-yoga/common';
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import merge from 'lodash/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { user, users, createUser } from '$lib/graphql/services/user.service';
 
 const sdls = Object.entries(import.meta.globEager('/src/lib/**/*.sdl.js')).map(([_, sdl]) => {
 	return sdl.schema;
@@ -26,7 +27,18 @@ export { yogaApp as get, yogaApp as post };
 
 function createSchema(sdls, services) {
 	const merged = mergeTypeDefs(sdls);
-	const schema = makeExecutableSchema({ typeDefs: merged });
+	const schema = makeExecutableSchema({
+		typeDefs: merged,
+		resolvers: {
+			Query: {
+				user: (root, args, context, info) => user(args, { root, context, info }),
+				users: (root, args, context, info) => users(args, { root, context, info })
+			},
+			Mutation: {
+				createUser: (root, args, context, info) => createUser(args, { root, context, info })
+			}
+		}
+	});
 	createResolversFromServices(schema, services, mergeResolvers(sdls));
 	return schema;
 }
